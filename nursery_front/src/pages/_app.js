f396b1d331b1cff7ed329a63de45650702d67f5d
suyeong <suyeong@viasoft.ai>
2023-08-23 18:21:16 +0900

    [Feat] 폰트설정 및 GlobalStyle 설정

--- nursery_front/src/pages/_app.js
+++ nursery_front/src/pages/_app.js
@@ -1,5 +1,28 @@
-import "@/styles/globals.css";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import SSRProvider from "react-bootstrap/SSRProvider";
+import BootstrapThemeProvider from "react-bootstrap/ThemeProvider";
+import GlobalStyle from "@src/styles/globalStyle";
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "@public/static/fonts/fontStyle.css";
+import theme from "@src/styles/theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SSRProvider>
+      <BootstrapThemeProvider lang="en" breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Head>
+            <title>농가용</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </BootstrapThemeProvider>
+    </SSRProvider>
+  );
 }
