9f998b45d840af7c0c9ea1ca55915ca7493c0d94
viachaeyeon <cykim@viasofts.com>
2023-09-11 15:30:07 +0900

    [Feat] 개요 파종기 가동시간 API 연결

--- manager_front/src/apis/planterAPIs.js
+++ manager_front/src/apis/planterAPIs.js
@@ -59,3 +59,18 @@ export const getDashboardFarmHouseAPI = async (queryType) => {
         throw new Error(err.response?.status || "농가별 생산량을 불러오는데 실패하였습니다. 잠시 후 다시 시도해주세요.");
       }
 }
+
+//개요페이지 : 파종기 가동시간
+export const getDashboardPlanterTimeAPI = async (queryType) => {
+    try {
+        const res = await axios.get(
+          process.env.NEXT_PUBLIC_END_POINT + `/api/admin/planter/planter/total-operating-time?query_type=${queryType}`,
+          {
+            withCredentials: true,
+          },
+        );
+        return res.data;
+      } catch (err) {
+        throw new Error(err.response?.status || "파종기 가동시간을 불러오는데 실패하였습니다. 잠시 후 다시 시도해주세요.");
+      }
+}
--- manager_front/src/utils/query-keys/PlanterQueryKeys.js
+++ manager_front/src/utils/query-keys/PlanterQueryKeys.js
@@ -2,3 +2,4 @@ export const PlanterStatusKey = "PlanterStatusKey";
 export const PlanterTotalKey = "PlanterTotalKey";
 export const PlanterCropKey = "PlanterCropKey";
 export const PlanterFarmKey = "PlanterFarmKey";
+export const PlanterOperatingTimeKey = "PlanterOperatingTimeKey";
