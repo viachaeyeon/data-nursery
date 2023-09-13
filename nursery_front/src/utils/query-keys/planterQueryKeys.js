92acda0c499fb9fff89cf8094456ec445ccae09c
viachaeyeon <cykim@viasofts.com>
2023-09-13 11:27:14 +0900

    [Chore] conflict

--- nursery_front/src/utils/query-keys/planterQueryKeys.js
+++ nursery_front/src/utils/query-keys/planterQueryKeys.js
@@ -1,1 +1,5 @@
-export const PlanterStatusKey = "PlanterStatusKey";
+export const trayListKey = "trayListKey";
+export const dashBoardKey = "dashBoardKey";
+export const workingWorkInfoKey = "workingWorkInfoKey";
+export const waitWorkListKey = "waitWorkListKey";
+export const workInfoKey = "workInfoKey";
