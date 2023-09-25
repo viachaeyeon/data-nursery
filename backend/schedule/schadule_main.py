2635335016dabba8149c5e9efce029b4f61f066c
viaDPBell <minjong@viasofts.com>
2023-09-25 19:29:28 +0900

    [Feat] 파종기 상태 30분동안 ON일경우 OFF로 변경

--- backend/schedule/schadule_main.py
+++ backend/schedule/schadule_main.py
@@ -1,12 +1,8 @@
-# from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-# Schedule = AsyncIOScheduler()
 from apscheduler.schedulers.background import BackgroundScheduler
-from sqlalchemy import func, or_
+from sqlalchemy import func, or_, insert
 
-# from sqlalchemy.orm import
-from datetime import datetime
-from utils.database import get_db
+from datetime import datetime, timedelta
+from utils.database import get_db, engine
 import src.planter.models as planterModels
 
 Schedule = BackgroundScheduler(timezone="Asia/Seoul")
@@ -38,16 +34,73 @@ def update_planter_work_deadline():
         db.commit()
 
 
+def update_planter_status_to_off():
+    db = next(get_db())
+
+    before_thirty = datetime.utcnow() - timedelta(minutes=30)
+
+    subq = (
+        db.query(
+            planterModels.PlanterStatus.planter_id,
+            func.max(planterModels.PlanterStatus.status).label("last_status"),
+            func.max(planterModels.PlanterStatus.updated_at).label("max_updated_at"),
+        )
+        .filter(
+            planterModels.PlanterStatus.is_del == False,
+        )
+        .group_by(
+            planterModels.PlanterStatus.planter_id,
+        )
+        .subquery()
+    )
+    planter_status = (
+        db.query(planterModels.PlanterStatus)
+        .join(
+            subq,
+            (planterModels.PlanterStatus.planter_id == subq.c.planter_id)
+            & (planterModels.PlanterStatus.updated_at == subq.c.max_updated_at),
+        )
+        .filter(
+            planterModels.PlanterStatus.status == "ON",
+            planterModels.PlanterStatus.updated_at <= before_thirty,
+        )
+        .all()
+    )
+
+    if planter_status:
+        # INFO: sqlalchemy core 사용
+        save_data = insert(planterModels.PlanterStatus).values(
+            [
+                {
+                    "planter_id": status.planter_id,
+                    "status": "OFF",
+                    "operating_time": status.operating_time,
+                }
+                for status in planter_status
+            ]
+        )
+        db.execute(save_data)
+        db.commit()
+
+
 # Schedule.add_job(
-#     update_planter_work_deadline,
+#     update_planter_status_to_off,
 #     "interval",
 #     seconds=3,
-#     id="update_planter_work_deadline",
+#     id="update_planter_status_to_off",
 # )
+
 Schedule.add_job(
     update_planter_work_deadline,
     "cron",
     hour="0",
     minute="0",
     id="update_planter_work_deadline",
+)
+
+Schedule.add_job(
+    update_planter_status_to_off,
+    "interval",
+    minutes=30,
+    id="update_planter_status_to_off",
 )
